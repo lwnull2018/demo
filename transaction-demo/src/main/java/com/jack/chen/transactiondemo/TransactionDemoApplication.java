34d5b06ac1235e78325d7da6588cc6eb2e90848e
zhang <lwnull2018@gmail.com>
2024-03-04 15:22:18 +0800

    @Transactional注解演示

--- transaction-demo/src/main/java/com/jack/chen/transactiondemo/TransactionDemoApplication.java
+++ transaction-demo/src/main/java/com/jack/chen/transactiondemo/TransactionDemoApplication.java
@@ -1,21 +1,15 @@
 package com.jack.chen.transactiondemo;
 
-import com.jack.chen.transactiondemo.service.DeptService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAspectJAutoProxy(exposeProxy = true)//如果不需要AopContext.currentProxy();可以不用这行注入
 public class TransactionDemoApplication {
 
-    private static ApplicationContext applicationContext;
-
     public static void main(String[] args) {
-        applicationContext = SpringApplication.run(TransactionDemoApplication.class, args);
-        Object bean = applicationContext.getBean(DeptService.class);
-        System.out.println("启动完成..." + bean);
+        SpringApplication.run(TransactionDemoApplication.class, args);
     }
 
 }
